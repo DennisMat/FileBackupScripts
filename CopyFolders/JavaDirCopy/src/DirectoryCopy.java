e883b568912a10f034d4ed24beaa1681b0968ee2
dennis <dennis.toronto.email@gmail.com>
2024-08-02 17:37:37 -0400

    Scripting in Java

--- CopyFolders/JavaDirCopy/src/DirectoryCopy.java
+++ CopyFolders/JavaDirCopy/src/DirectoryCopy.java
@@ -8,105 +8,115 @@ import java.util.ArrayList;
 
 public class DirectoryCopy {
 
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out
+					.println("Usage: java DirectoryCopy.java <source> <destination> <log_file> <array of exclusions>");
+			return;
+		}
 
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java DirectoryCopy <source> <target> <logfile> <excludeFolder1> <excludeFolder2> ...");
-            return;
-        }
+		Path source = Paths.get(args[0]);
+		Path target = Paths.get(args[1]);
+		Path logFile = Paths.get(args[2]);
 
-        Path source = Paths.get(args[0]);
-        Path target = Paths.get(args[1]);
-        Path logFile = Paths.get(args[2]);
+		List<String> excludeFolders = new ArrayList<>();
+		for (int i = 3; i < args.length; i++) {
+			excludeFolders.add(File.separator + args[i] + File.separator);
+		}
 
-        List<String> excludeFolders = new ArrayList<>();
-        for (int i = 3; i < args.length; i++) {
-            excludeFolders.add(args[i]);
-        }
+		try {
+			copyDirectory(source, target, excludeFolders, logFile);
+			System.out.println("Directory copy operation completed successfully.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        try {
-            copyDirectory(source, target, excludeFolders, logFile);
-            System.out.println("Directory copy operation completed successfully.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static void copyDirectory(Path source, Path target, List<String> excludeFolders, Path logFile) throws IOException {
-        List<Path> extraFilesInTarget = new ArrayList<>();
+	public static void copyDirectory(Path source, Path target, List<String> excludeFolders, Path logFile)
+			throws IOException {
+		List<Path> extraFilesInTarget = new ArrayList<>();
 
-        // Walk the source tree
-        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                Path relativePath = source.relativize(dir);
-                if (excludeFolders.contains(relativePath.toString())) {
-                	System.out.println("Exclude folder = " + relativePath);
-                    return FileVisitResult.SKIP_SUBTREE;
-                }
-                Path targetDir = target.resolve(relativePath);
-                if (!Files.exists(targetDir)) {
-                    Files.createDirectories(targetDir);
-                }
-                return FileVisitResult.CONTINUE;
-            }
+		// Walk the source tree
+		Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				Path relativePath = source.relativize(dir);
+				if (excludeFolders.contains(relativePath.toString())) {
+					System.out.println("Exclude folder = " + relativePath);
+					return FileVisitResult.SKIP_SUBTREE;
+				}
+				Path targetDir = target.resolve(relativePath);
+				if (!Files.exists(targetDir)) {
+					Files.createDirectories(targetDir);
+				}
+				return FileVisitResult.CONTINUE;
+			}
 
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                Path relativePath = source.relativize(file);
-                Path targetFile = target.resolve(relativePath);
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Path relativePath = source.relativize(file);
+				Path targetFile = target.resolve(relativePath);
 
-                if (Files.exists(targetFile)) {
-                    FileTime sourceTime = Files.getLastModifiedTime(file);
-                    FileTime targetTime = Files.getLastModifiedTime(targetFile);
+				if (Files.exists(targetFile)) {
+					FileTime sourceTime = Files.getLastModifiedTime(file);
+					FileTime targetTime = Files.getLastModifiedTime(targetFile);
 
-                    if (sourceTime.compareTo(targetTime) > 0) {
-                    	System.out.println("newer timestamp file = " + file.toString());
-                    	Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
-                    }
-                } else {
-                    Files.copy(file, targetFile);
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        });
+					if (sourceTime.compareTo(targetTime) > 0) {
+						System.out.println("newer timestamp file = " + file.toString());
+						try {
+							Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
+						} catch (Exception e) {
+							System.err.println(e.getMessage());
+						}
+					}
+				} else {
+					try {
+						Files.copy(file, targetFile);
+					} catch (Exception e) {
+						System.err.println(e.getMessage());
+					}
+				}
+				return FileVisitResult.CONTINUE;
+			}
+		});
 
-        // Walk the target tree to find extra files/folders
-        Files.walkFileTree(target, new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                Path relativePath = target.relativize(file);
-                Path sourceFile = source.resolve(relativePath);
-                if (!Files.exists(sourceFile)) {
-                    extraFilesInTarget.add(file);
-                }
-                return FileVisitResult.CONTINUE;
-            }
+		// Walk the target tree to find extra files/folders
+		Files.walkFileTree(target, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Path relativePath = target.relativize(file);
+				Path sourceFile = source.resolve(relativePath);
+				if (!Files.exists(sourceFile)) {
+					extraFilesInTarget.add(file);
+				}
+				return FileVisitResult.CONTINUE;
+			}
 
-            @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                Path relativePath = target.relativize(dir);
-                Path sourceDir = source.resolve(relativePath);
-                if (!Files.exists(sourceDir)) {
-                    extraFilesInTarget.add(dir);
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        });
+			@Override
+			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				Path relativePath = target.relativize(dir);
+				Path sourceDir = source.resolve(relativePath);
+				if (!Files.exists(sourceDir)) {
+					extraFilesInTarget.add(dir);
+				}
+				return FileVisitResult.CONTINUE;
+			}
+		});
 
-        writeToLog(logFile, extraFilesInTarget);
-    }
+		writeToLog(logFile, extraFilesInTarget);
+	}
 
 	private static void writeToLog(Path logFile, List<Path> extraFilesInTarget) throws IOException {
 		// Log the extra files/folders
-        try (BufferedWriter logWriter = Files.newBufferedWriter(logFile, StandardOpenOption.CREATE, StandardOpenOption.APPEND)) {
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            logWriter.write("Log Time: " + sdf.format(System.currentTimeMillis()) + "\n");
-            logWriter.write("Extra files/folders in target directory:\n");
-            for (Path extraFile : extraFilesInTarget) {
-                logWriter.write(extraFile.toString() + "\n");
-            }
-            logWriter.write("\n");
-        }
+		try (BufferedWriter logWriter = Files.newBufferedWriter(logFile, StandardOpenOption.CREATE,
+				StandardOpenOption.APPEND)) {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			logWriter.write("Log Time: " + sdf.format(System.currentTimeMillis()) + "\n");
+			logWriter.write("Extra files/folders in target directory:\n");
+			for (Path extraFile : extraFilesInTarget) {
+				logWriter.write(extraFile.toString() + "\n");
+			}
+			logWriter.write("\n");
+		}
 	}
 }
--- CopyFolders/JavaDirCopy/src/TakeBackupJava.bat
+++ CopyFolders/JavaDirCopy/src/TakeBackupJava.bat
@@ -1,2 +1,7 @@
-java DirectoryCopy.java "C:/dennis" "D:/dennis" "C:\Users\admin\Desktop\Shortcuts\backup.log" ".git" ".settings" "target"
+echo "Started copying C:/dennis to D:/dennis" 
+java DirectoryCopy.java "C:/dennis" "D:/dennis" "C:\Users\admin\Desktop\Shortcuts\backup.log" ".git" ".class" ".settings" "target" "bin"
+echo "Done copying C:/dennis to D:/dennis" 
+
+echo "Started copying D:/dennis to \\Dennisthinkcent\d\dennis"
+java DirectoryCopy.java "D:/dennis" "\\Dennisthinkcent\d\dennis" "C:\Users\admin\Desktop\Shortcuts\backup.log" ".git" ".class" ".settings" "target" "bin"
 pause
